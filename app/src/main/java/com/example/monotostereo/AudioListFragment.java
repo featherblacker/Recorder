7376b6c2713914311d47d63fbee25e4658c221e6
featherblacker <chuqiguang@gmail.com>
2020-06-28 13:18:45 -0400

    usable

--- app/src/main/java/com/example/monotostereo/AudioListFragment.java
+++ app/src/main/java/com/example/monotostereo/AudioListFragment.java
@@ -1,64 +1,205 @@
 package com.example.monotostereo;
 
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AudioListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AudioListFragment extends Fragment {
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.io.File;
+import java.io.IOException;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+public class AudioListFragment extends Fragment implements AudioListAdapter.onItemListClick {
+
+    private ConstraintLayout playerSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private RecyclerView recyclerView;
+    private File[] allFiles;
+    private AudioListAdapter audioListAdapter;
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
+    private File fileToPlay;
+    private ImageView playBtn;
+    private TextView playerHeader;
+    private TextView playerFileName;
+    private SeekBar playerSeekBar;
+    private Handler seekBarHandler;
+    private Runnable updateSeekBar;
 
     public AudioListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AudioListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AudioListFragment newInstance(String param1, String param2) {
-        AudioListFragment fragment = new AudioListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_audio_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        playerSheet = view.findViewById(R.id.player_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
+        recyclerView = view.findViewById(R.id.audio_list_view);
+        playBtn = view.findViewById(R.id.player_play_btn);
+        playerFileName = view.findViewById(R.id.player_file_name);
+        playerHeader = view.findViewById(R.id.player_header_title);
+        playerSeekBar = view.findViewById(R.id.player_seek_bar);
+
+        String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        File directory = new File(path);
+        allFiles = directory.listFiles();
+        audioListAdapter = new AudioListAdapter(allFiles, this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(audioListAdapter);
+
+
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                if (isPlaying) {
+                    pauseAudio();
+                } else {
+                    if (fileToPlay != null) {
+                        resumeAudio();
+                    }
+                }
+            }
+        });
+        playerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if(fileToPlay!=null){
+                    pauseAudio();
+                }
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(fileToPlay!=null){
+                    int progress = seekBar.getProgress();
+                    mediaPlayer.seekTo(progress);
+                    resumeAudio();
+                }
+
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClickListener(File file, int position) {
+        fileToPlay = file;
+        if (isPlaying) {
+            stopAudio();
+        }
+        playAudio(fileToPlay);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void pauseAudio() {
+        mediaPlayer.pause();
+        playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_play_btn, null));
+        isPlaying = false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void resumeAudio() {
+        mediaPlayer.start();
+        playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_pause_btn, null));
+        isPlaying = true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void stopAudio() {
+        playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_play_btn, null));
+        isPlaying = false;
+        playerHeader.setText("Stopped");
+        mediaPlayer.stop();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void playAudio(File fileToPlay) {
+        mediaPlayer = new MediaPlayer();
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+        try {
+            mediaPlayer.setDataSource((fileToPlay.getAbsolutePath()));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        playBtn.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.player_pause_btn, null));
+        playerFileName.setText(fileToPlay.getName());
+        playerHeader.setText("Playing");
+        isPlaying = true;
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopAudio();
+                playerHeader.setText("Finished");
+            }
+        });
+
+        playerSeekBar.setMax(mediaPlayer.getDuration());
+        seekBarHandler = new Handler();
+        updateSeekBar = new Runnable() {
+            @Override
+            public void run() {
+                playerSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+                seekBarHandler.postDelayed(this, 500);
+            }
+        };
+        seekBarHandler.postDelayed(updateSeekBar, 0);
     }
 }
--- app/src/main/java/com/example/monotostereo/RecordFragment.java
+++ app/src/main/java/com/example/monotostereo/RecordFragment.java
@@ -1,59 +1,53 @@
 package com.example.monotostereo;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RecordFragment extends Fragment {
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RecordFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int PERMISSION_CODE = 21;
+    private NavController navController;
+    private ImageView list_Btn;
+    private ImageView recordBtn;
+    private boolean isRecording = false;
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
+    private Chronometer timer;
+    private TextView filenameText;
+
 
     public RecordFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecordFragment newInstance(String param1, String param2) {
-        RecordFragment fragment = new RecordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,4 +55,84 @@ public class RecordFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_record, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        list_Btn = view.findViewById(R.id.record_list_button);
+        recordBtn = view.findViewById(R.id.record_button);
+        timer = view.findViewById(R.id.record_timer);
+        filenameText = view.findViewById(R.id.record_filename);
+
+        list_Btn.setOnClickListener(this);
+        recordBtn.setOnClickListener(this);
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.record_list_button:
+                navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+                break;
+            case R.id.record_button:
+                if (isRecording){
+                    stopRecording();
+                    recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_stopped, null));
+                    isRecording = false;
+                }else{
+                    if(checkPermission()){
+                        startRecording();
+                        recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_recording, null));
+                        isRecording = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    private void stopRecording() {
+        timer.stop();
+        filenameText.setText("Recording stopped, file saved");
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private void startRecording() {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        recordFile = "Recording_"+formatter.format(now)+".wav";
+        filenameText.setText("Recording, file name: "+recordFile);
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(recordPath+"/"+recordFile);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaRecorder.start();
+    }
+
+    private boolean checkPermission() {
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recordPermission}, PERMISSION_CODE);
+            return false;
+        }
+    }
+
 }
