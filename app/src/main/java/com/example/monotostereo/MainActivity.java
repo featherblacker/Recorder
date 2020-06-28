0a8120f205ac383a0cb09433f4caaf908cc19a8d
featherblacker <chuqiguang@gmail.com>
2020-06-27 20:40:04 -0400

    90

---
+++ app/src/main/java/com/example/monotostereo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.monotostereo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
